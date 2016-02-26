9fe63c1465149b135358a79bb8587acc1f1f7ba1
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-02-26 22:17:39 +0000

    added sound to buttons

--- retro-space-calculator/ViewController.swift
+++ retro-space-calculator/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftValString = ""
+    var rightValString = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        
+        let soundURL = NSURL(fileURLWithPath: path!)
+        
+        do {
+           try btnSound = AVAudioPlayer(contentsOfURL: soundURL)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func numberPressed(btn: UIButton!) {
+        btnSound.play()
     }
 
+    @IBAction func onDividePressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+    }
+    
 
 }
 
